6ad01b34c4d9b72db50147fcbb8cc3b45986ce94
Omar Ebrahim Mohamed Khalil <200012464@must.edu.eg>
2024-07-25 03:33:57 +0300

    Assignment

--- Program.cs
+++ Program.cs
@@ -2,9 +2,50 @@
 {
     internal class Program
     {
+        public static void BubbleSort(int[] x)
+        {
+            int temp = 0;
+            #region Q1
+            if (x is not null)
+            {
+                for (int i = 0; i < x.Length; i++)
+                {
+                    for (int j = 0; j < x.Length - i - 1; j++)
+                    {
+                        if (x[j] > x[j + 1])
+                        {
+                            temp = x[j];
+                            x[j] = x[j + 1];
+                            x[j + 1] = temp;
+                        }
+                    }
+
+                }
+            } 
+            #endregion
+
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1
+            //int[] x = { 1, 3, 2, 0 };
+            //BubbleSort(x);
+            //foreach (int xe in x) { Console.WriteLine(xe); } 
+            #endregion
+            #region Part 2
+            #region q1
+            Range<int> range = new Range<int>(2, 6);
+            #endregion
+            #region Q2
+            //range.IsInRange(3);
+            //Console.WriteLine($"3 is in range? {range.IsInRange(3)}");
+            #endregion
+            #region Q3
+            //int x=range.Legth();
+            //Console.WriteLine($"Length between Maximum and Minmum: {x}");
+            #endregion
+            #endregion
+
         }
     }
 }
---
+++ Range.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics1
+{
+    internal class Range<T> where T :IComparable<T>
+    {
+        public T Min { get; private set; }
+        public T Max { get; private set; }
+
+        public Range(T min, T max)
+        {
+            if(min.CompareTo(max)> 0) { Console.WriteLine("invalid range"); }
+            Min = min;
+            Max = max;
+        }
+
+        public bool IsInRange(T value)
+        {
+            return value.CompareTo(Min) >= 0 && value.CompareTo(Max) <= 0;
+        }
+        public T Legth()
+        {
+            dynamic min=Min;
+            dynamic max=Max;
+            return max-min;
+        }
+
+    }
+}
